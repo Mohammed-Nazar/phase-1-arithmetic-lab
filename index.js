048dd86d4eaa068accdaead76aa062f82da2c657
Mohammed Nazar <x3raqe@gmail.com>
2024-02-16 15:41:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 
 // Write your code here
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
+
+let random = Number.parseInt(((Math.random() + 1) *10));
+
+let num3 = 9;
+let num4 = 5;
+let mod = 9 % 5;
+
+let max = Math.max(10,20,18);
